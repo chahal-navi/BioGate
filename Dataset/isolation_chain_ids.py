e2a3dec253a2babd08ac9ba50a510e9c1b390406
chahal-navi <nnavi4806@gmail.com>
2026-04-26 00:59:22 +0530

    Create isolation_chain_ids.py

---
+++ Dataset/isolation_chain_ids.py
@@ -0,0 +1,36 @@
+# The Master Malign Chain Map
+
+chain_map_malign = {
+    # SARS-CoV-2 & MERS Spike RBDs (Highly dependent on bound receptors)
+    '6M0J': 'E', '7BZ5': 'A', '6VW1': 'E', '6LZG': 'B', '7RBY': 'C',
+    '7KLW': 'A', '7KMB': 'G', '7LM9': 'A', '7KGK': 'A', '5X4S': 'A',
+    '6W41': 'C', '8K3K': 'E', '7D2Z': 'B', '7JYC': 'A', '7KFV': 'A',
+    '7LXX': 'A', '7JVB': 'B', '8ZER': 'B', '2GHV': 'C', '8YDV': 'D',
+    '7T9L': 'A', '7VNB': 'B', '7W9I': 'E', '7XBY': 'B', '7EAM': 'A',
+    
+    # Influenza Hemagglutinin (Usually crystallized as Chain A)
+    '1RVZ': 'A', '4O5N': 'A', '2VSM': 'A', '4L72': 'B', '6HJN': 'B',
+    '1RU7': 'A', '1RD8': 'A', '5UG0': 'A', '2FK0': 'A', '4QY1': 'A',
+    '4O5I': 'A', '4JUG': 'A', '4JUL': 'A', '4ZMJ': 'B', '3AL4': 'A',
+    
+    # Nipah, Hendra, RSV, and emerging viruses
+    '5JQ3': 'A', '8JR5': 'A', '4JHW': 'F', '5W23': 'A', '6TYS': 'A',
+    '4KR0': 'B', '4KQZ': 'A', '4N5B': 'A', '5X4R': 'A', '3BGF': 'A'
+}
+
+chain_map_benign_1 = {
+    '1IGT' : 'A', '1IGY' :'A', '5M2J': 'D', '6AL5': ['H', 'L'], '7LFB':['H', 'L'],
+    '7FAB': ['H', 'L'], '4W70': 'A', '4TYU':'A', '6APO':'A', '4LAS':'H', 
+    '1I3V':'A', '1OP9':'A', '4LAR':'H', '1BZQ':'K',
+    '1NQB':['A', 'C'], '1YQV':['L', 'H'], '2IG2':['H', 'L']
+}
+
+chain_map_benign_2 = {
+    '1TIM':'A', '2LYZ':'A', '1AKE':'A', '3PGK':'A', '1MBO':'A', '1FKB':'A', '1G8Q':'A', '4F80':'A',
+    '3RRQ':'A', '6OIL':'A', '4F8T':'A', '4J6K':'A', '2PTN':'A', '1RNH':'A', 
+    '2X29':'A', '1XED':'A', '4F8Q':'A'
+}
+chain_map_benign_3 = {
+    '7S5B':'A', '8U5L':'F', '5TRV':'A', '9XZT':'A', '6W40':'A', '8JPA':'A', '6WI5':'A', '9RGX':'A', 
+    '8KDQ':'A', '8HDU':'A', '8W97':'A', '8K8G':'A', '8KA7':'A', '5CW9':'A', '8K84':'A', '5CWG':'A'
+}
